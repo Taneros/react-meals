0c542d97ad3d2ec80b23b21d9bdcc399414960fb
Taneros <fricentaurus@GMAIL.COM>
2022-08-23 09:02:07 +0300

    fix: bugs

--- src/components/Meals/MealItem/MealItemForm.jsx
+++ src/components/Meals/MealItem/MealItemForm.jsx
@@ -1,11 +1,33 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import { Input } from '../../UI/Input'
 import styles from './MealItemForm.module.css'
 
-export const MealItemForm = () => {
+export const MealItemForm = (props) => {
+  const [amountIsValid, setAmountIsValid] = useState(true)
+
+  const submitHandler = (event) => {
+    event.preventDefault()
+
+    const enteredAmount = amountInputRef.current.value
+
+    if (
+      enteredAmount.trim().length === 0 ||
+      enteredAmount < 1 ||
+      enteredAmount > 5
+    ) {
+      setAmountIsValid(false)
+      return
+    }
+
+    props.addToCart(Number(enteredAmount))
+  }
+
+  const amountInputRef = useRef(null)
+
   return (
-    <form className={styles.form}>
+    <form className={styles.form} onSubmit={submitHandler}>
       <Input
+        ref={amountInputRef}
         label="Amount"
         input={{
           id: 'amount',
@@ -17,6 +39,7 @@ export const MealItemForm = () => {
         }}
       />
       <button>Add</button>
+      {!amountIsValid && <p>Please enter a valid amount</p>}
     </form>
   )
 }
